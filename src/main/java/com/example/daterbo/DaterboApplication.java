e2c8d07542c02237f71155b2c5861e7c841c2f97
Agas <agasp435@gmail.com>
2025-08-02 14:23:32 +0700

    first commit

---
+++ src/main/java/com/example/daterbo/DaterboApplication.java
@@ -0,0 +1,13 @@
+package com.example.daterbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaterboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaterboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/daterbo/DaterboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daterbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaterboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
